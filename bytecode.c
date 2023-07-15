2428733f6d56bd6e402c5c62b380f3fb306b609b
Clare <me@clare.fyi>
2023-07-15 17:13:46 -0400

    add line numbers

--- bytecode.c
+++ bytecode.c
@@ -6,22 +6,26 @@ void InitBytecode(bytecode_t* bc) {
   bc->count = 0;
   bc->capacity = 0;
   bc->code = NULL;
+  bc->lines = NULL;
   InitValueArray(&bc->constants);
 }
 
-void WriteBytecode(bytecode_t* bc, u8 byte) {
+void WriteBytecode(bytecode_t* bc, u8 byte, u32 line) {
   if (bc->capacity < bc->count + 1) {
     u32 oldCapacity = bc->capacity;
     bc->capacity = GROW_CAPACITY(oldCapacity);
     bc->code = GROW_ARRAY(u8, bc->code, oldCapacity, bc->capacity);
+    bc->lines = GROW_ARRAY(u32, bc->lines, oldCapacity, bc->capacity);
   }
 
   bc->code[bc->count] = byte;
+  bc->lines[bc->count] = line;
   bc->count++;
 }
 
 void FreeBytecode(bytecode_t* bc) {
   FREE_ARRAY(u8, bc->code, bc->capacity);
+  FREE_ARRAY(u32, bc->lines, bc->capacity);
   FreeValueArray(&bc->constants);
   InitBytecode(bc);
 }
--- bytecode.h
+++ bytecode.h
@@ -14,11 +14,12 @@ typedef struct {
   u32 count;
   u32 capacity;
   u8* code;
+  u32* lines;
   value_array_t constants;
 } bytecode_t;
 
 void InitBytecode(bytecode_t* bc);
-void WriteBytecode(bytecode_t* bc, u8 byte);
+void WriteBytecode(bytecode_t* bc, u8 byte, u32 line);
 void FreeBytecode(bytecode_t* bc);
 i32  AddConstant(bytecode_t* bc, value_t val);
 
